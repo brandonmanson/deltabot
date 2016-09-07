48b7948c07066cb87b2752c5de5e47e013046fc9
Brandon Manson <brandon.manson@detroitlabs.com>
2016-09-07 16:08:32 -0400

    added spotify script

---
+++ scripts/spotify.js
@@ -0,0 +1,19 @@
+var SpotifyWebApi = require('spotify-web-api-node');
+
+var spotifyApi = new SpotifyWebApi({
+  clientId: '54b550eaad0748afb6577403a2b09b6a',
+  slientSecret: '416e48a5cabf4eb6b006dd876b64c534',
+  redirectUri: ''
+});
+
+module.exports = function(robot) {
+  robot.respond(/jam to (.*)/i, function(msg) {
+    var artist = msg.match[1];
+    spotifyApi.searchArtists(artist)
+    .then(function(data) {
+      var parsedJson = JSON.parse(data.body);
+      var url = parsedJson.artists.items[0].external_urls.spotify;
+      msg.reply(url);
+    })
+  })
+}
