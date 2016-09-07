f8276a5c24c5645e0c7a012e1028e1d7d306a4cd
Brandon Manson <brandon.manson@detroitlabs.com>
2016-09-07 18:50:26 -0400

    copied and pasted code from docs

--- scripts/spotify.js
+++ scripts/spotify.js
@@ -10,14 +10,20 @@ module.exports = function(robot) {
   robot.respond(/jam to (.*)/i, function(msg) {
     var artist = msg.match[1];
     console.log(artist);
-    spotifyApi.searchArtists('Eminem')
-    .then(function(data) {
-      var parsedJson = JSON.parse(data.body);
-      var url = parsedJson.artists.items[0].external_urls.spotify;
-      console.log("Search artists by eminem", data.body);
-      msg.reply(artist);
-    }, function(err) {
-      console.error(err);
-    });
+    spotifyApi.searchArtists('Love')
+      .then(function(data) {
+          console.log('Search artists by "Love"', data.body);
+        }, function(err) {
+          console.error(err);
+        });
+    // spotifyApi.searchArtists('Eminem')
+    // .then(function(data) {
+    //   // var parsedJson = JSON.parse(data.body);
+    //   // var url = parsedJson.artists.items[0].external_urls.spotify;
+    //   console.log("Search artists by eminem", data.body);
+    //   msg.reply(artist);
+    // }, function(err) {
+    //   console.error(err);
+    // });
   });
 }
