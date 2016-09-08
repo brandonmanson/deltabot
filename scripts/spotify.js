2da09f832c7138d9b7d7c26ae261221908f6c2f7
Brandon Manson <brandon.manson@detroitlabs.com>
2016-09-08 14:13:20 -0400

    changed bot to integrate with youtube

--- scripts/spotify.js
+++ scripts/spotify.js
@@ -1,25 +1,17 @@
-var SpotifyWebApi = require('spotify-web-api-node');
-
-var spotifyApi = new SpotifyWebApi();
-
 module.exports = function(robot) {
   robot.respond(/jam to (.*)/i, function(msg) {
     var artist = msg.match[1];
-    console.log(artist);
-    spotifyApi.searchTracks('Love')
-      .then(function(data) {
-          console.log('Search artists by "Love"', data.body);
-        }, function(err) {
-          console.error(err);
-        });
-    // spotifyApi.searchArtists('Eminem')
-    // .then(function(data) {
-    //   // var parsedJson = JSON.parse(data.body);
-    //   // var url = parsedJson.artists.items[0].external_urls.spotify;
-    //   console.log("Search artists by eminem", data.body);
-    //   msg.reply(artist);
-    // }, function(err) {
-    //   console.error(err);
-    // });
+    var partOne = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=';
+    var partTwo = '&type=playlist&key=AIzaSyD6EcOnDFc1JB2lBGtDq2FhRpqKVySroK4';
+    robot.http(partOne + artist + partTwo).get(function(err, res, body) {
+      if (err) {
+        console.log(err);
+      }
+      data = JSON.parse(body);
+      id = data.items.id.playlistId;
+      url = "https://youtube.com/playlist?list=" + id;
+      res.send(url);
+    })
+
   });
 }
