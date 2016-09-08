084ff0652186aabf5fc7b29c12be3a2295cbde54
Brandon Manson <brandon.manson@detroitlabs.com>
2016-09-08 14:25:31 -0400

    ready to test

--- scripts/spotify.js
+++ scripts/spotify.js
@@ -3,15 +3,16 @@ module.exports = function(robot) {
     var artist = msg.match[1];
     var partOne = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=';
     var partTwo = '&type=playlist&key=AIzaSyD6EcOnDFc1JB2lBGtDq2FhRpqKVySroK4';
-    robot.http(partOne + artist + partTwo).get(function(err, res, body) {
+    robot.http(partOne + artist + partTwo).get()(function(err, res, body) {
       if (err) {
         console.log(err);
       }
       console.log(body);
       data = JSON.parse(body);
-      id = data.items.id.playlistId;
+      id = data.items[0].id.playlistId;
       url = "https://youtube.com/playlist?list=" + id;
-      res.send(url);
+      console.log(url);
+      msg.send(url);
     })
 
   });
