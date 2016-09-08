28a1128b031c1f5a08185929bfbf55f1abb3a409
Brandon Manson <brandon.manson@detroitlabs.com>
2016-09-08 14:39:31 -0400

    changed to do one random video

--- scripts/spotify.js
+++ scripts/spotify.js
@@ -1,16 +1,22 @@
+function getRandomIndex() {
+  return Math.floor(Math.random() * (30 - 0 + 1)) + 0;
+}
+
 module.exports = function(robot) {
   robot.respond(/jam to (.*)/i, function(msg) {
     var artist = msg.match[1];
-    var partOne = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=';
-    var partTwo = '&type=playlist&key=AIzaSyD6EcOnDFc1JB2lBGtDq2FhRpqKVySroK4';
+    var partOne = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=30&q=';
+    var partTwo = '&type=video&key=AIzaSyD6EcOnDFc1JB2lBGtDq2FhRpqKVySroK4';
     robot.http(partOne + artist + partTwo).get()(function(err, res, body) {
       if (err) {
         console.log(err);
       }
       console.log(body);
       data = JSON.parse(body);
-      id = data.items[0].id.playlistId;
-      url = "https://youtube.com/playlist?list=" + id;
+      randomIndex = getRandomIndex();
+      console.log("Random Index: " + randomIndex);
+      id = data.items[randomIndex].id.videoId;
+      url = "https://youtube.com/watch?v=" + id;
       console.log(url);
       msg.send(url);
     })
