942d66d8a9864617ec94b04c74fb2fc18c31669f
Brandon Manson <brandon.manson@detroitlabs.com>
2016-09-08 15:06:35 -0400

    obfuscated api key

--- scripts/spotify.js
+++ scripts/spotify.js
@@ -6,7 +6,8 @@ module.exports = function(robot) {
   robot.respond(/jam to (.*)/i, function(msg) {
     var artist = msg.match[1];
     var partOne = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=30&q=';
-    var partTwo = '&type=video&key=AIzaSyD6EcOnDFc1JB2lBGtDq2FhRpqKVySroK4';
+    var apiKey = process.env.apiKey;
+    var partTwo = '&type=video&key=' + apiKey;
     robot.http(partOne + artist + partTwo).get()(function(err, res, body) {
       if (err) {
         console.log(err);
